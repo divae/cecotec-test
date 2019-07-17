c10420eb0ab74b5ecdc770722eb51bf16a558264
Estela Medrano <stlmedrano@gmail.com>
2019-07-17 21:52:44 +0200

    usuario administrable en admin con permisos

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -4,15 +4,33 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-
+    
     def test_create_user_with_email_successful(self):
-        """Test creating a new user with an email is successful"""
-        email = 'test@test.com'
-        password = 'Password123'
-        user = get_user_model().objects.create_user(
-            email=email,
-            password=password
-        )
+        email = 'stlmedrano@gmail.com'
+        password = 'test123'
+
+        user = sample_user(email, password)
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = 'stlmedrano@GMAIL.com'
+        test_password = 'test123'
+        user = get_user_model().objects.create_user(email, test_password)
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        test_password = 'test123'
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, test_password)
+
+    def test_create_new_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'sfs@gmail.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
